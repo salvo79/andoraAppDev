41aeb064b8c462e1ac9298f8339bc04d05b98890
salvo79 <salvo79@users.noreply.github.com>
2026-04-03 18:17:27 +0000

    deploy: 91e083c931c01a6a6b394db245c37181b943b28b

---
+++ assets/index-CFw883m_.js
@@ -0,0 +1,21 @@
+import{ek as t,o as n,b as s,a,eo as o,en as r,el as l}from"./index-DktgB2Va.js";var d=`
+    .p-toolbar {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        flex-wrap: wrap;
+        padding: dt('toolbar.padding');
+        background: dt('toolbar.background');
+        border: 1px solid dt('toolbar.border.color');
+        color: dt('toolbar.color');
+        border-radius: dt('toolbar.border.radius');
+        gap: dt('toolbar.gap');
+    }
+
+    .p-toolbar-start,
+    .p-toolbar-center,
+    .p-toolbar-end {
+        display: flex;
+        align-items: center;
+    }
+`,p={root:"p-toolbar p-component",start:"p-toolbar-start",center:"p-toolbar-center",end:"p-toolbar-end"},b=t.extend({name:"toolbar",style:d,classes:p}),i={name:"BaseToolbar",extends:l,props:{ariaLabelledby:{type:String,default:null}},style:b,provide:function(){return{$pcToolbar:this,$parentInstance:this}}},c={name:"Toolbar",extends:i,inheritAttrs:!1},m=["aria-labelledby"];function y(e,u,v,$,g,f){return n(),s("div",r({class:e.cx("root"),role:"toolbar","aria-labelledby":e.ariaLabelledby},e.ptmi("root")),[a("div",r({class:e.cx("start")},e.ptm("start")),[o(e.$slots,"start")],16),a("div",r({class:e.cx("center")},e.ptm("center")),[o(e.$slots,"center")],16),a("div",r({class:e.cx("end")},e.ptm("end")),[o(e.$slots,"end")],16)],16,m)}c.render=y;export{c as s};
