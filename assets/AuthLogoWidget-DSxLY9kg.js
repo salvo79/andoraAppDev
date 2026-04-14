bac22a55177c2800c42b9fef34f494f97825b254
salvo79 <salvo79@users.noreply.github.com>
2026-04-14 23:31:12 +0000

    deploy: 34f366b7888dc00443ac7ac4945d24e6b21968c6

---
+++ assets/AuthLogoWidget-DSxLY9kg.js
@@ -0,0 +1,1 @@
+import{o as e,b as o,e as s}from"./index-CsnGhCEk.js";const r=["src"],l={__name:"AuthLogoWidget",setup(t){const a="/andoraAppDev/";return(n,c)=>(e(),o("img",{src:`${s(a)}andora.svg`,alt:"anDora",class:"h-52"},null,8,r))}};export{l as _};
---
+++ assets/Index-5kiE5gqQ.js
@@ -0,0 +1,1 @@
+import{c1 as o,o as c,c as r,c2 as n}from"./index-CsnGhCEk.js";const t={};function s(_,a){const e=n("router-view");return c(),r(e)}const p=o(t,[["render",s]]);export{p as default};
